0ba60082e3abe1c8bd6e8368931e9258215b896e
Maria Sussan <mariasussan17@gmail.com>
2026-03-11 00:00:01 +0000

    simple form

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,72 @@ import React from "react"
 
 export default function App() {
 
-    const [isGoingOut, setIsGoingOut]= React.useState(false);
 
 
-    function ansButton(){
+    function signUp(formData){
 
-        setIsGoingOut(prevCheck => !prevCheck);
+        const email     =   formData.get("emailId");
+        const password  =   formData.get("password");
+        const aboutMe   =   formData.get("aboutMe");
+        const gender    =   formData.get("gender");
+        const skills    =   formData.getAll("skills");
+        const favColor  =   formData.get("favColor");
 
+        console.log(favColor);
     }
     return(
 
-        <main className ="container">
-            <h1>Is going out?</h1>  
-            <div className ="counter">
-                <button onClick={ansButton}>{isGoingOut? "Yes":"No"}</button>
-            </div>
-   
-     
-        </main>
+        <section>
+            <form action={signUp}>
+
+                <h1>Sign Up Form</h1>
+                <label>
+                  Email <input type="email" id="emailId" name="emailId"/>
+                </label><br/>
+                <label>
+                   Password <input type="password" id="password" name="password"/>
+                </label><br/>
+                <label>
+                   About Me<br/>
+                <textarea id="aboutMe" name="aboutMe"></textarea>
+                </label><br/>
+                <fieldset>
+                    <legend>Gender</legend>
+                 <label>
+                    <input type="radio" name="gender" value="Male"/>Male
+                </label>
+                <label>
+                    <input type="radio" name="gender" value="Female"/>Female
+                </label>
+                </fieldset>
+                <fieldset>
+                    <legend>Skills</legend>
+                 <label>
+                    <input type="checkbox" name="skills" value="HTML" />HTML
+                </label>
+                <label>
+                    <input type="checkbox" name="skills" value="CSS"/>CSS
+                </label>
+                <label>
+                    <input type="checkbox" name="skills" value="React"/>React
+                </label>
+                </fieldset>   
+                <label>
+                 Favorite colour   <select id="favColor" name="favColor" >
+                        <option value="" ></option>
+                        <option value="red">Red</option>
+                        <option value="orange">Orange</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                        <option value="indigo">Indigo</option>
+                        <option value="violet">Violet</option>
+                    </select>
+                </label>
+                <button>Submit</button>
+            </form>
+
+        </section>
     )
 }
 
